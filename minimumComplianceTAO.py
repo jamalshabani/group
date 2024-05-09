d00afeae7018f9eeea22c62fcdf2147150f1daab
jamalshabani <jjuma1992@gmail.com>
2024-05-09 14:14:21 -0400

    “Changes”

--- minimumComplianceTAO.py
+++ minimumComplianceTAO.py
@@ -67,7 +67,8 @@ _lambda = (E * nu)/((1 + nu) * (1 - 2 * nu))
 def h(rho):
 	return delta * (1 - rho)**2 + rho**2
 
-# Define W(a) function i.e W(x) = x(1 - x)
+# Define double-well potential W(a) function i.e W(x) = x(1 - x) use lagrange = 5.0
+# Define double-well potential W(a) function i.e W(x) = x^2(1 - x)^2 use lagrange = 5.0
 def W(rho):
 	return pow(rho, 2) * pow((1 - rho), 2)
 
